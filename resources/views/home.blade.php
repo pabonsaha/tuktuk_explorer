e69f132140fe016894732abea56c3f369413cb94
Rayuhri <rayhan603688@gmail.com>
2026-04-01 17:50:12 +0800

    title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 
+@section('title', 'Lisbon TukTuk Explorer | Guided Tuk Tuk Tours in Lisbon')
 
 
 @push('styles')
@@ -36,7 +37,6 @@
         }
     </style>
 @endpush
-@section('title', 'Lisbon TukTuk Explorer | Guided Tuk Tuk Tours in Lisbon')
 
 @section('content')
     <section class="banner-slider h-[50vh] md:h-screen relative"
